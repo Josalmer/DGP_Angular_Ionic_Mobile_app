6a815bdc6a978f1866aa84f4af614639ffaa0c42
Manuel <manuelpcastro@correo.ugr.es>
2020-11-03 20:00:42 +0100

    Arreglado scroll en la lista de grupos

--- src/app/shared/services/my-groups.service.ts
+++ src/app/shared/services/my-groups.service.ts
@@ -4,31 +4,33 @@ import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MyGroupsService {
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) {}
 
-  getGroups(): Observable<any> {
-    return this.http.get('groups');
-  }
+	getGroups(): Observable<any> {
+		return this.http.get('groups');
+	}
 
-  getGroupById(id: string): Observable<any> {
-    return this.http.get('groups/' + id);
-  }
+	getGroupById(id: string): Observable<any> {
+		return this.http.get('groups/' + id);
+	}
 
-  getMockGroups(): Observable<any> {
-    const groups = [
-      { name: 'Grupo Actividades 1', id: 1, participants: 31, messages: 10 },
-      { name: 'Grupo Apoyo 3', id: 2, participants: 20, messages: 5 },
-      { name: 'Grupo Centro', id: 3, participants: 53, messages: 45 },
-      { name: 'Grupo La Chana', id: 4, participants: 45, messages: 32 }
-    ];
+	getMockGroups(): Observable<any> {
+		const groups = [
+			{ name: 'Grupo Actividades 1', id: 1, participants: 31, messages: 10 },
+			{ name: 'Grupo Apoyo 3', id: 2, participants: 20, messages: 5 },
+			{ name: 'Grupo Centro', id: 3, participants: 53, messages: 45 },
+			{ name: 'Grupo La Chana', id: 4, participants: 45, messages: 32 },
+			{ name: 'Grupo Baile', id: 5, participants: 13, messages: 45 },
+			{ name: 'Grupo Matemáticas', id: 6, participants: 45, messages: 33 }
+		];
 
-    return of(groups);
-  }
+		return of(groups);
+	}
 
-  getMockGroupById(id: string): Observable<any> {
-    return of({ name: 'Grupo Actividades 1', id: 1, participants: 31, messages: 10 });
-  }
+	getMockGroupById(id: string): Observable<any> {
+		return of({ name: 'Grupo Actividades 1', id: 1, participants: 31, messages: 10 });
+	}
 }

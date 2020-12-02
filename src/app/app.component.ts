a2b8d60c0b815234ff7cfca83bc218352dbfdb1e
Jose Saldaña <jose.saldana@nazaries.com>
2020-12-02 07:33:21 +0100

    Added push notificacions with FCM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NotificationsService } from './shared/services/notifications.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -12,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private notificationsService: NotificationsService
   ) {
     this.initializeApp();
   }
@@ -20,6 +22,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.notificationsService.initPushNotifications();
       this.splashScreen.hide();
     });
   }
--- src/app/my-profile/pages/my-profile/components/user-menu/user-menu.component.ts
+++ src/app/my-profile/pages/my-profile/components/user-menu/user-menu.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { LoginService } from 'src/app/login/services/login.service';
+import { MyProfileService } from 'src/app/shared/services/my-profile.service';
+import { SessionService } from 'src/app/shared/services/session.service';
 
 @Component({
-	selector: 'app-user-menu',
-	templateUrl: 'user-menu.component.html'
+  selector: 'app-user-menu',
+  templateUrl: 'user-menu.component.html'
 })
 export class userMenuComponent {
-	constructor(
-		private router: Router
-	) {}
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private loginService: LoginService,
+    private sessionService: SessionService,
+    private profileService: MyProfileService
+  ) { }
 
-	navigateToUserProgress() {
-		this.router.navigateByUrl('/tabs/my-profile/user-progress');
-	}
+  navigateToUserProgress() {
+    this.router.navigateByUrl('/tabs/my-profile/user-progress');
+  }
+
+  async logout() {
+    this.loginService.logout().subscribe((response) => {
+      this.sessionService.clearAuthToken();
+      this.profileService.clearUser();
+      this.navCtrl.navigateRoot(['/login']);
+    });
+  }
 }
